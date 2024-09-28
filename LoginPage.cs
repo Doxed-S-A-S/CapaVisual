d4cb08c087a577857d45ea5b81c137adca0dd00a
SebastianMoreira06 <smoreira2@estudiante.ceibal.edu.uy>
2024-09-28 16:32:55 -0300

    Creación De Login
    Creado el LoginPage

---
+++ LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVisual
+{
+    public partial class LoginPage : MaterialForm
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkin.MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new MaterialSkin.ColorScheme(
+                MaterialSkin.Primary.Red600,
+                MaterialSkin.Primary.Red700,
+                MaterialSkin.Primary.Red200,
+                MaterialSkin.Accent.Green400,
+                MaterialSkin.TextShade.BLACK
+                );
+        }
+
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
